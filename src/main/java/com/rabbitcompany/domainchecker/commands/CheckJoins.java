5a053a475272809392681508cfa8d441026db96d
BuildTools <unconfigured@null.spigotmc.org>
2021-07-31 19:56:00 +0200

    - Added total joins

--- src/main/java/com/rabbitcompany/domainchecker/commands/CheckJoins.java
+++ src/main/java/com/rabbitcompany/domainchecker/commands/CheckJoins.java
@@ -18,9 +18,14 @@ public class CheckJoins extends Command {
 
         if(!sender.hasPermission("domainchecker.checkjoins")) return;
 
+        int sum = 0;
+
         for(String domain : Domains.domains.keySet()){
             String message = DomainChecker.config.getString("check_joins", "{subdomain}.tulipsurvival.com: {unique_joins}");
-            sender.sendMessage(new ComponentBuilder(message.replace("{subdomain}", domain).replace("{unique_joins}", ""+Domains.domains_temp.getOrDefault(domain, 0))).color(ChatColor.GREEN).create());
+            int joins = Domains.domains_temp.getOrDefault(domain, 0);
+            sum += joins;
+            sender.sendMessage(new ComponentBuilder(message.replace("{subdomain}", domain).replace("{unique_joins}", ""+joins)).color(ChatColor.GREEN).create());
         }
+        sender.sendMessage((new ComponentBuilder("Total: " + sum).color(ChatColor.GREEN).create()));
     }
 }
